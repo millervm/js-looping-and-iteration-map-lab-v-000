bd2f36baa11cd571dc99d7200d2e6d7da9d45fbf
millervm <vmcmiller@gmail.com>
2019-01-30 18:03:12 +0000

    Done.

--- index.js
+++ index.js
@@ -14,5 +14,4 @@ function nameToAttributes(drivers) {
 function attributesToPhrase(drivers) {
     return drivers.map(function(driver) {return `${driver.name} is from ${driver.hometown}`;
     });
-    
 }
