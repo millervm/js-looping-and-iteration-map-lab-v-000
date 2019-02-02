087811e1293be4d71992e013fa91b2ed7fa4d8fd
Veronica Miller <vmcmiller@gmail.com>
2019-02-02 17:50:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // Code your solution in this file.
+<<<<<<< HEAD
 function lowerCaseDrivers(drivers) {
     return drivers.map(function(driver) {return driver.toLowerCase();
     });
@@ -15,3 +16,12 @@ function attributesToPhrase(drivers) {
     return drivers.map(function(driver) {return `${driver.name} is from ${driver.hometown}`;
     });
 }
+=======
+function lowerCaseDrivers(drivers ) {
+  return drivers.map(function(driver) {return driver.toLowerCase(); });
+}
+
+function attributesToPhrase(drivers) {
+  return drivers.map(function(driver) {return '${driver.name} is from ${driver.hometown}';});
+}
+>>>>>>> 90b3960948293dabc7541c4b1baf670e8974faf8
